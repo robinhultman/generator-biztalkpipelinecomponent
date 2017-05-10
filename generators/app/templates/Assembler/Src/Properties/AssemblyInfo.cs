71226bed43ceb94491bd4bd64d56ee8cc1e9cd27
Robin Hultman <robin.hultman@gmail.com>
2017-05-10 11:51:31 +0200

    Fixed problem with Title and product
    Component name placeholder was wrong and thus missing in the generated files

--- generators/app/templates/Assembler/Src/Properties/AssemblyInfo.cs
+++ generators/app/templates/Assembler/Src/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("<%= namespace %>.<% name %>")]
+[assembly: AssemblyTitle("<%= namespace %>.<%= name %>")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("<%= namespace %>.<% name %>")]
+[assembly: AssemblyProduct("<%= namespace %>.<%= name %>")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
